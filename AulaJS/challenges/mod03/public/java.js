08e319152bff1b63c9a22a0d342b34cad0ab989f
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-04-15 18:12:16 -0300

    Update java.js
    modal erased
    open in the same page as courses (server not working)

--- AulaJS/challenges/mod03/public/java.js
+++ AulaJS/challenges/mod03/public/java.js
@@ -6,6 +6,17 @@ const modal = document.querySelector(".modal")
 for (let card of cards){
     card.addEventListener("click", function() {
         const url = card.getAttribute("id")
+        window.location.href = `courses/:id=${url} `
+    })
+
+}
+
+
+
+/*  COM MODAL
+for (let card of cards){
+    card.addEventListener("click", function() {
+        const url = card.getAttribute("id")
         overlay.classList.add('active')
         overlay.querySelector("iframe").src = `https://rocketseat.com.br/${url}`
     })
@@ -28,40 +39,5 @@ maxiModal.addEventListener("click", function(){
         modal.classList.remove("max")
         
     }
-})
-
-/*
-const modaloverlay = document.querySelector('.modal-overlay')
-const cards = document.querySelectorAll('.card')
-// const maximodal= document.querySelector('.maximize-modal')
-// const modal = document.querySelector('.modal')
-
-
-for (let card of cards){
-    card.addEventListener("click", function(){
-        const page= card.getAttribute("id")
-        modaloverlay.classList.add('active')
-        modaloverlay.querySelector("iframe").src = `https://rocketseat.com.br/${page}`
-    })
-}
-
-document.querySelector(".close-modal"),addEventListener("click", function(){
-    modaloverlay.classList.remove('active')
-    modaloverlay.querySelector("iframe").src=""
-    // modal.classList.remove("max")
-    // maximodal.classList.remove('up')
-})
-
-
-maximodal.addEventListener("click", function(){
-   if ( modal.classList.contains("max") == false) {
-      modal.classList.add("max")
-      maximodal.classList.add("max")
-   }
-   else{
-       modal.classList.remove("max")
-       maximodal.classList.remove("max")
-   }
-    
 })
 */
