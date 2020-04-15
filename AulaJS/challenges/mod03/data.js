c4947755a4154c6f9680a5176e2c4d76646fae9c
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-04-15 18:14:12 -0300

    Update data.js

--- AulaJS/challenges/mod03/data.js
+++ AulaJS/challenges/mod03/data.js
@@ -2,6 +2,7 @@ module.exports = [
     {
         id: "starter",
         img: "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
+        url: "https://rocketseat.com.br/",
         title: "Starter",
         duration: "3 modulos",
         price: "Free"
@@ -10,6 +11,7 @@ module.exports = [
     {
         id: "launchbase",
         img: "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg" ,
+        url: "https://rocketseat.com.br/",
         title: "Launchbase",
         duration: "3 modulos",
         price: "R$1.000,00"
@@ -18,6 +20,7 @@ module.exports = [
     {
         id: "gostack",
         img: "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
+        url: "https://rocketseat.com.br/",
         title: "GoStack",
         duration: "3 modulos",
         price: "R$1.000,00"
