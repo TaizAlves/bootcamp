70842cfd43e7bd7510cedd1a102071a6229e3334
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-04-15 21:42:32 -0300

    Create server.js

---
+++ AulaJS/challenges/foodfy2/server.js
@@ -0,0 +1,35 @@
+const express= require("express")
+const nunjucks = require("nunjucks")
+
+const server = express()
+
+const receitas = require("./data")
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express:server
+})
+
+
+server.get("/", function(req,res){
+    return res.render("index")
+})
+
+
+server.get("/receitas", function(req,res){
+    return res.render("receitas", {items:receitas})
+})
+
+server.get("/sobre", function(req,res){
+    return res.render("sobre")
+})
+
+
+server.listen(5000, function(){
+    console.log("server is running")
+})
+
+
