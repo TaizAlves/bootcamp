49ec3fadf54875c9be467aad688bbb33ab641f86
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-04-17 21:35:37 -0300

    Update scriptfoodfy.js

--- AulaJS/challenges/foodfy2/public/scriptfoodfy.js
+++ AulaJS/challenges/foodfy2/public/scriptfoodfy.js
@@ -1,5 +1,48 @@
-const modalover = document.querySelector(".modal-over");
 const recipes = document.querySelectorAll(".recipe");
+
+
+for (let recipe of recipes){
+    recipe.addEventListener("click", function(){
+        const recipeId= recipe.getAttribute("id")
+        window.location.href = `/receitas/${recipeId}`
+    })      
+}
+
+
+const showhide = document.getElementsByClassName("top")
+
+
+for (let button of showhide){
+    const buttonshowhide = querySelector("button")
+
+    buttonshowhide.addEventListener("click", function(){
+        if (buttonshowhide.innerHTML == "esconder"){
+            button.querySelector(".content_detail").classList.add('hidden')
+            buttonshowhide.innerHTML = "mostrar"
+        }
+        else {
+            buttonshowhide.querySelector(".content_detail").classList.remove('hidden')
+            buttonshowhide.innerHTML = "esconder"
+        }
+
+        if (buttonSpan.innerHTML == "ESCONDER") {
+            showHide.querySelector('.topic-content').classList.add('hidden');
+            buttonSpan.innerHTML = "MOSTRAR"   
+          } else {
+            showHide.querySelector('.topic-content').classList.remove('hidden');
+            buttonSpan.innerHTML = "ESCONDER"
+          }
+    })
+}
+
+
+
+
+
+/*  1fase= com modal
+
+const modalover = document.querySelector(".modal-over");
+const recipes = document.querySelectorAll(".recipe")
 
 for (let recipe of recipes){
     recipe.addEventListener("click", function(){
@@ -18,4 +61,4 @@ document.querySelector(".close-modal").addEventListener("click", function(){
     modalover.classList.remove('active')
 })
 
-
+*/
