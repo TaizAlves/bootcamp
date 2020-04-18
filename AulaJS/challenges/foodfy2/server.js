afae325d7f0cc8efa91004edb2d9523862ed7e02
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-04-17 21:34:29 -0300

    Update server.js
    new page to open recipe

--- AulaJS/challenges/foodfy2/server.js
+++ AulaJS/challenges/foodfy2/server.js
@@ -10,13 +10,16 @@ server.use(express.static('public'))
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express:server
+    express:server,
+    autoescape: false
 })
 
 
 server.get("/", function(req,res){
-    return res.render("index")
+        
+    return res.render("index", {items:receitas})
 })
+
 
 
 server.get("/receitas", function(req,res){
@@ -27,9 +30,21 @@ server.get("/sobre", function(req,res){
     return res.render("sobre")
 })
 
+server.get("/receitas/:index", function(req,res){
+    const recipeIndex = req.params.index
+    //console.log(receitas[recipeIndex])
+    return res.render("detalhe", {item:receitas[recipeIndex]})
+
+})
+
 
 server.listen(5000, function(){
     console.log("server is running")
 })
+
+
+
+
+
 
 
