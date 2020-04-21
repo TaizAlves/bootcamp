9dc8f476141cae3791d0ac16ec27328a0c57bcc8
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-04-21 20:35:10 -0300

    Update routes.js
    add.post (forms)

--- AulaJS/AulaHtml/modulo04/routes.js
+++ AulaJS/AulaHtml/modulo04/routes.js
@@ -6,8 +6,25 @@ routes.get('/', function(req,res) {
     return res.redirect("/instructors")
 })
 
+routes.get('/instructors/create', function(req,res){
+    return res.render("instructors/create")
+
+})
+
 routes.get('/instructors', function(req,res){
     return res.render("instructors/index")
+})
+
+routes.post("/instructors", function(req,res){
+   
+    const keys = Object.keys(req.body) // cria uma lista das chaves 
+    // req.body = retorna uma lista do que foi preenchido junto com a chave
+    for (key of keys){
+         if (req.body[key] == "")
+            return res.send("Please, fill all fields!")
+    }
+
+    return res.send(req.body)
 })
 
 routes.get('/members', function(req,res){
