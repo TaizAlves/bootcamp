b95f7d7c96596991219885673f5432454738e3a8
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-04-22 22:59:55 -0300

    Update routes.js
    create route POST

--- AulaJS/challenges/mod04/routes.js
+++ AulaJS/challenges/mod04/routes.js
@@ -1,19 +1,30 @@
 const express = require('express')
 const routes = express.Router()
+const teacher = require('./teacher')
+
+
 
 routes.get('/', function(req,res){
     return res.redirect("/teachers")
 })
 
+
 routes.get('/teachers', function (req,res){
     return res.render("teachers/index")
 })
+
+
+routes.get('/teachers/create', function(req,res){
+    return res.render("teachers/create")
+})
+
+
 
 routes.get('/students', function(req,res){
     return res.send ("oi")
 })
 
 
-
+routes.post("/teachers", teacher.post)
 
 module.exports = routes
