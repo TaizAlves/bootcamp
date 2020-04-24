ddf5dcd3dbc07b8e7d75f265f9ca7e51fb0e04ec
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-04-24 17:18:08 -0300

    Update teacher.js
    export.edit

--- AulaJS/challenges/mod04/teacher.js
+++ AulaJS/challenges/mod04/teacher.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const data = require ('./data.json')
+const {age, graduation} = require('./utils')
 
 exports.post = function(req, res) {
     const keys = Object.keys(req.body)
@@ -9,7 +10,7 @@ exports.post = function(req, res) {
         return res.send ("Please fill blanked fields")
     }
 
-    req.body.created_at = Date.prototype.toDateString
+    req.body.created_at = Date.now()
     req.body.id = Number(data.teachers.length +1)
 
     data.teachers.push(req.body)
@@ -20,4 +21,75 @@ exports.post = function(req, res) {
         
     })
     return res.redirect("./teachers")
+}
+
+
+exports.show= function (req,res){
+    const {id}=  req.params
+    const foundTeacher = data.teachers.find(function(teacher){
+        return teacher.id == id
+    })
+    if (!foundTeacher) return res.send("Teacher not found")
+
+    const teacher = {
+        ...foundTeacher,
+        age: age(foundTeacher.birth),
+        graduation: graduation(foundTeacher.graduation),
+        subject: foundTeacher.subject.split(','),
+        created_at: new Intl.DateTimeFormat('pt-BR').format(foundTeacher.created_at)
+       
+
+    }
+    return res.render("teachers/show", {teacher})
+}
+
+exports.edit = function(req,res){
+    const {id} = req.params
+    const foundTeacher = data.teachers.find(function(teacher){
+        return teacher.id == id
+    })
+
+    if (!foundTeacher)  return res.send("Teacher not found")
+
+    const teacher = {
+        ...foundTeacher,
+        graduation: graduation(foundTeacher.graduation),
+        
+
+    }
+    return res.render('teachers/edit', {teacher})
+}
+
+
+
+exports.put = function(req,res){
+
+    const {id} = req.body
+
+    let index = 0
+
+    const foundTeacher = data.teachers.find(function(teacher, foundindex){
+
+        if (id==teacher.id){
+            index = foundindex
+            return true
+        }
+    })
+
+    if(!foundTeacher) return res.send("Teacher not found")
+
+    const teacher ={
+        ...foundTeacher,
+        ...req.body
+    }
+
+    data.teachers[index]=teacher
+
+    fs.writeFile("data.json", JSON.stringify(data,null,4), function(err){
+        if (err) return res.send("Write error")
+
+    })
+    
+    return res.redirect(`/teachers/${id}`)
+
 }
