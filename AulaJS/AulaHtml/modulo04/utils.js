6fa1a858cc122f8cff64843109252e44f6302d9f
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-04-23 21:09:15 -0300

    Create utils.js
    functions

---
+++ AulaJS/AulaHtml/modulo04/utils.js
@@ -0,0 +1,30 @@
+module.exports = {
+    age:function(timestamp){
+            const today = new Date()
+            const birthDate = new Date(timestamp)
+        
+            let age =today.getFullYear() - birthDate.getFullYear()
+            const month = today.getMonth() - birthDate.getMonth()
+        
+            if (month < 0 || month ==0 && today.getDate() < birthDate.getDate() ) {
+                age = age -1
+            }
+            return age
+        },
+    
+
+
+    date: function(timestamp){
+        const date = new Date(timestamp)
+
+        const year = date.getUTCFullYear() // UTC para ser universal
+
+        const month = `0${date.getUTCMonth() +1}`.slice(-2)// p.q é de 0 a 11
+
+        const day = `0${date.getUTCDate()}`.slice(-2)
+
+        return `${year}-${month}-${day}`
+
+
+    }
+}
