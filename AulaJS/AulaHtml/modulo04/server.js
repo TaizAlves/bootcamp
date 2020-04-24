6382ce24e08e2afe2239726890fe33397aa84d8c
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-04-24 19:14:40 -0300

    Update server.js
    add method-override

--- AulaJS/AulaHtml/modulo04/server.js
+++ AulaJS/AulaHtml/modulo04/server.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 const routes = require("./routes") //usar o routes
+const methodoverride = require('method-override')
 
 
 const server = express()
 
 server.use(express.urlencoded({extended:true}))  // faz funcionar o req.body lá do post em routes
 
-
 server.use(express.static('public'))
+server.use(methodoverride('_method'))
 server.use(routes)
 
 
