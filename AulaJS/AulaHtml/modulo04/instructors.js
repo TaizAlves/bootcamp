81152f4d5d316272b5ef140e7a2ca90ef22570eb
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-04-23 21:02:55 -0300

    Update instructors.js
    edit
    put

--- AulaJS/AulaHtml/modulo04/instructors.js
+++ AulaJS/AulaHtml/modulo04/instructors.js
@@ -1,5 +1,30 @@
 const fs = require ('fs')
 const data = require ("./data.json")
+const {age, date} = require('./utils')
+
+
+
+// show
+exports.show = function(req,res){
+    //req.params
+    const {id} = req.params
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id ==id
+    })
+    if (!foundInstructor) return res.send ("Instructor not found")
+
+    
+
+    const instructor = {
+        ...foundInstructor, // coloca tudo o que tem dentro do foundInstructor
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(','),
+        created_at: new Intl.DateTimeFormat('en-GB').format(foundInstructor.created_at) ,
+    }
+
+    return res.render("instructors/show", {instructor})
+}
+
 
 
 
@@ -49,8 +74,53 @@ exports.post = function(req,res){
 }
 
 
-//uppdate
+// edit
+exports.edit = function(req,res){
+    const {id} = req.params
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id ==id
+    })
+    if (!foundInstructor) return res.send ("Instructor not found")
+
+    const instructor ={
+        ...foundInstructor,
+        birth:date(foundInstructor.birth)
+    }
+    
+    return res.render ('instructors/edit', {instructor})
+}
 
 
-//delete
+//update - atualizar PUT (salvar no backend)
+
+exports.put = function(req,res){
+
+    const {id} = req.body
+
+    let index = 0
+
+    const foundInstructor = data.instructors.find(function(instructor, foundindex){
+        if (id == instructor.id){
+            index= foundindex
+            return true
+        }
+        
+    })
+    if (!foundInstructor) return res.send ("Instructor not found")
+
+    const instructor ={
+        ...foundInstructor,
+        ...req.body,
+        birth = Date.parse(req.body.birth)
+    }
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null,2), function(err){
+        if (err) return res.send("Write error")
+
+        return res.redirect(`/instructors/${id}`)
+    })
+}
+
+
 
