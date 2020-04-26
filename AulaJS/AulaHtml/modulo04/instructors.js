39228337fc695c8e37910263abb0c31e70d2e70d
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-04-26 15:43:50 -0300

    Update instructors.js
    add. index

--- AulaJS/AulaHtml/modulo04/instructors.js
+++ AulaJS/AulaHtml/modulo04/instructors.js
@@ -2,6 +2,10 @@ const fs = require ('fs')
 const data = require ("./data.json")
 const {age, date} = require('./utils')
 
+exports.index = function(req,res){
+    return res.render("instructors/index", {instructors:data.instructors})
+}
+
 
 
 // show
@@ -85,6 +89,7 @@ exports.edit = function(req,res){
     const instructor ={
         ...foundInstructor,
         birth:date(foundInstructor.birth)
+        
     }
     
     return res.render ('instructors/edit', {instructor})
@@ -111,7 +116,9 @@ exports.put = function(req,res){
     const instructor ={
         ...foundInstructor,
         ...req.body,
-        birth: Date.parse(req.body.birth)
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
+
     }
 
     data.instructors[index] = instructor
