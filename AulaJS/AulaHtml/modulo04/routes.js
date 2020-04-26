55191727e4782e940c4a1d82dde9c83af3bdec4e
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-04-26 15:44:08 -0300

    Update routes.js
    chande index

--- AulaJS/AulaHtml/modulo04/routes.js
+++ AulaJS/AulaHtml/modulo04/routes.js
@@ -12,9 +12,7 @@ routes.get('/instructors/create', function(req,res){
 
 })
 
-routes.get('/instructors', function(req,res){
-    return res.render("instructors/index")
-})
+routes.get('/instructors',instructors.index )
 
 routes.get("/instructors/:id", instructors.show)
 
