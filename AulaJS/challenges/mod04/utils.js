fa9b16b325f9fbe0ff67e10ec847f5da8e7fb496
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-04-28 11:32:58 -0300

    Update utils.js

--- AulaJS/challenges/mod04/utils.js
+++ AulaJS/challenges/mod04/utils.js
@@ -14,27 +14,53 @@ module.exports ={
 
     },
 
-    graduation: function(level){
 
-        if (level  == "elementary"){
-            return "Ensino Médio Completo"
+    gradeshow: function(level){
+
+        if (level  == "5ano"){
+            return "5º ano do ensino médio"
         }
-        else if (level == "degree"){
-            return "Ensino Superior Completo"
+        else if (level == "6ano"){
+            return "6º ano do ensino médio"
         }
-        else if (level == "Master") {
-            return "Mestrado"
+        else if (level == "7ano") {
+            return "7º ano do ensino médio"
+        }
+        else if (level == "8ano") {
+            return "8º ano do ensino médio"
+        }
+        else if (level == "9ano") {
+            return "9º ano do ensino médio"
+        }
+        else if (level == "1colegial") {
+            return "1º ano do colegial"
+        }
+        else if (level == "2colegial") {
+            return "2º ano do colegial"
         }
         else {
-            return "Doutorado"
+            return "3º ano do colegial"
         }
 
+    },
+
+    date: function(timestamp){
+        const date = new Date(timestamp)
+
+        const year = date.getUTCFullYear()
+
+        const month = `0${date.getUTCMonth()+1}`.slice(-2)
+
+        const day = `0${date.getUTCDate()}`.slice(-2)
+
+        return {
+            day,
+            month,
+            year,
+            iso: `${year}-${month}-${day}`,
+            birthDay: `${day}/${month}`
+        }
     }
-
-
-
-
-
 
 
 }
