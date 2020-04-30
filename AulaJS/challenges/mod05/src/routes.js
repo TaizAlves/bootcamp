0d22166dcc80041f58cfbee8cce2c7dad848cf52
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-04-29 21:12:06 -0300

    Create routes.js

---
+++ AulaJS/challenges/mod05/src/routes.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const routes = express.Router()
+const teacher = require('./app/controllers/teacher')
+const student = require('./app/controllers/student')
+
+
+
+routes.get('/', function(req,res){
+    return res.redirect("/teachers")
+})
+
+routes.get('/teachers', teacher.index)
+
+routes.get('/teachers/create', teacher.create)
+
+routes.get('/teachers/:id', teacher.show)
+
+routes.get('/teachers/:id/edit', teacher.edit)
+
+routes.put('/teachers', teacher.put)
+
+routes.post("/teachers", teacher.post)
+
+routes.delete("/teachers", teacher.delete)
+
+
+
+routes.get('/students', student.index)
+
+routes.get('/students/create', student.create)
+
+routes.get('/students/:id', student.show)
+
+routes.get('/students/:id/edit', student.edit)
+
+routes.put('/students', student.put)
+
+routes.post("/students", student.post)
+
+routes.delete("/students", student.delete)
+
+
+
+
+
+
+
+
+
+module.exports = routes
