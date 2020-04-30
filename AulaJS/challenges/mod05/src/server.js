f7a2961544168397d0092c04abf327bb7117f71d
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-04-29 21:11:48 -0300

    Create server.js

---
+++ AulaJS/challenges/mod05/src/server.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const nunjucks = require("nunjucks")
+const routes = require("./routes")
+const methodOverride= require('method-override')
+
+const server = express()
+
+server.use(express.urlencoded({extended:true})) //para funionar o req.body
+server.use(methodOverride('_method')) // tem que ser antes de routes
+server.use(routes)
+server.use(express.static("public"))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("src/app/views", {
+    express:server,
+    autoescape: false, //para que a html dentro de uma variável funcione
+    noCache: true
+})
+
+
+
+
+
+
+
+
+server.listen(5000, function(){
+    console.log("server is running")
+})
