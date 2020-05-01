23db60db79f364ceba21b7f1b9cb6deb8a10a396
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-05-01 18:45:16 -0300

    Update Member.js
    update find, all, update

--- AulaJS/AulaHtml/modulo05/src/app/models/Member.js
+++ AulaJS/AulaHtml/modulo05/src/app/models/Member.js
@@ -26,8 +26,9 @@ module.exports ={
             birth,
             blood,
             weight,
-            height
-        ) VALUES ($1, $2,$3,$4,$5,$6,$7,$8)
+            height,
+            instructor_id
+        ) VALUES ($1, $2,$3,$4,$5,$6,$7,$8,$9)
         RETURNING id
 
         `
@@ -39,7 +40,8 @@ module.exports ={
             date(data.birth).iso,
             data.blood,
             data.weight,
-            data.height
+            data.height,
+            data.instructor
         ]
         
         db.query(query,values, function(err,results){
@@ -50,12 +52,15 @@ module.exports ={
     },
 
     find(id,callback){
-        db.query(`SELECT * FROM members WHERE id = $1`,[id], function(err,results){
+        db.query(`
+        SELECT members.* , instructors.name AS instructor_name
+        FROM members 
+        LEFT JOIN instructors ON (members.instructor_id = instructors.id)
+        WHERE members.id = $1`,[id], function(err,results){
             if(err) throw `Database error! ${err}`
             callback(results.rows[0])
 
         })
-
 
     },
 
@@ -69,8 +74,9 @@ module.exports ={
             email =($5),
             blood =($6),
             weight =($7),
-            height =($8)
-        WHERE id = $9
+            height =($8),
+            instructor_id =($9)
+        WHERE id = $10
         `
 
         const values = [
@@ -82,6 +88,7 @@ module.exports ={
             data.blood,
             data.weight,
             data.height,
+            data.instructor,
             data.id
         ]
 
@@ -100,7 +107,16 @@ module.exports ={
             
             return callback()
         })
+    },
+
+    instructorsSelectOptions(callback){
+        db.query (`SELECT name, id FROM instructors`, function(err,results){
+            if (err) throw `Database error ${err}`
+
+            callback(results.rows)
+        })
     }
+
 
 
 }
