da3788cfa45ecf0e167fe911dab0f0b27e5bc31b
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-05-03 19:06:41 -0300

    Update utils.js

--- AulaJS/challenges/mod05/src/lib/utils.js
+++ AulaJS/challenges/mod05/src/lib/utils.js
@@ -1,13 +1,13 @@
 module.exports ={
 
-    age: function(timestamp){
+    age(timestamp){
        const today = new Date()
        const birthday = new Date(timestamp)
 
        let age = today.getFullYear() - birthday.getFullYear()
        const month = today.getMonth() - birthday.getMonth()
 
-       if (month <0 || month ==0 && today.getDate() < birthday.getDate()){
+       if (month <0 || month ==0 && today.getDate() < birthday.getDate() ){
            age= age-1
        }
        return age
@@ -15,7 +15,7 @@ module.exports ={
     },
 
 
-    gradeshow: function(level){
+    gradeshow(level){
 
         if (level  == "5ano"){
             return "5º ano do ensino médio"
@@ -38,29 +38,54 @@ module.exports ={
         else if (level == "2colegial") {
             return "2º ano do colegial"
         }
-        else {
+        else if (level == "3colegial") {
             return "3º ano do colegial"
+        }
+        else if (level == "elementary") {
+            return "Ensino Médio Completo"
+        }
+        else if (level == "degree") {
+            return "Ensino Superior Completo"
+        }
+        else if (level == "master") {
+            return "Mestrado"
+        }
+        
+        else {
+            return "Doutorado"
         }
 
     },
 
-    date: function(timestamp){
+
+    class_type(local){
+        if (local == "à distância"){
+            return "à distância"
+        }
+        else {
+            return "presencial"
+        }
+
+    },
+
+    date(timestamp){
         const date = new Date(timestamp)
 
-        const year = date.getUTCFullYear()
+        const year = date.getUTCFullYear() 
 
-        const month = `0${date.getUTCMonth()+1}`.slice(-2)
+        const month = `0${date.getUTCMonth() +1}`.slice(-2)
 
         const day = `0${date.getUTCDate()}`.slice(-2)
 
         return {
-            day,
-            month,
-            year,
-            iso: `${year}-${month}-${day}`,
-            birthDay: `${day}/${month}`,
-            format: `${day}/${month}/${year}`
-        }
+        day,
+        month,
+        year,
+        iso:`${year}-${month}-${day}`,
+        birthDay: `${day}/${month}`,
+        format: `${day}/${month}/${year}`
+
+    }
     }
 
 
