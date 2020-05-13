8f1a8ff05d6f5ea047f78dc12f459ed82fa3d32e
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-05-13 20:26:57 -0300

    Create server.js

---
+++ AulaJS/AulaHtml/modulo06/src/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const routes = require("./routes") //usar o routes
+const methodoverride = require('method-override')
+
+
+const server = express()
+
+server.use(express.urlencoded({extended:true}))  // faz funcionar o req.body lá do post em routes
+
+server.use(express.static('public'))
+server.use(methodoverride('_method'))
+server.use(routes)
+
+
+server.set("view engine", "njk")
+
+nunjucks.configure("src/app/views", {
+    express: server,
+    autoescape: false, //para que a html dentro de uma variável funcione
+    noCache: true
+})
+
+
+
+server.listen(5000, function()  {
+    console.log("server is running")
+
+})
+
+
