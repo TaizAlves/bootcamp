a2ee17a631cac98ca0d222155bb110701c41f79b
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-05-28 11:53:21 -0300

    Update routes.js

--- AulaJS/AulaHtml/modulo06/src/routes.js
+++ AulaJS/AulaHtml/modulo06/src/routes.js
@@ -2,12 +2,18 @@ const express = require('express')
 const routes = express.Router()  // responsavel pelas rotas
 const multer = require('./app/middlewares/multer')
 const ProductController = require('./app/controllers/ProductController')
+const HomeController = require('./app/controllers/HomeController')
+const SearchController = require('./app/controllers/SearchController')
 
-routes.get('/', function(req,res) {
+//HOME
+routes.get('/', HomeController.index)
 
-    return res.render("layout.njk")
-})
+// SEARCH 
 
+routes.get('/products/search', SearchController.index)
+
+
+// Products
 routes.get('/products/create', ProductController.create)
 routes.get('/products/:id', ProductController.show)
 routes.get('/products/:id/edit', ProductController.edit)
@@ -15,6 +21,8 @@ routes.get('/products/:id/edit', ProductController.edit)
 routes.post('/products',multer.array("photos",6) ,ProductController.post)
 routes.put('/products',multer.array("photos",6), ProductController.put)
 routes.delete('/products', ProductController.delete)
+
+
 
 //ALIAS - atalhos
 routes.get('/ads/create', function(req,res) {
