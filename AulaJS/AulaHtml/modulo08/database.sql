5793d28aca8b4387a75a5a1a258f3ecf25ac7e35
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-07-27 11:49:17 -0300

    Update database.sql

--- AulaJS/AulaHtml/modulo08/database.sql
+++ AulaJS/AulaHtml/modulo08/database.sql
@@ -143,3 +143,25 @@ CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON orders
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
+
+
+-- SOFT DELETE
+--1. Criar uma columna na table products chamada "deleted_at"
+ALTER TABLE products ADD COLUMN "deleted_at" timestamp;
+
+--2. Criar uma Regra que vai rodar todas as vexes que solicitarmos o DELETE
+CREATE OR REPLACE RULE delete_product AS 
+ON DELETE TO  products DO INSTEAD
+UPDATE products
+SET deleted_at = now()
+WHERE products.id = old.id;
+
+
+--3. Criar uma VIEW onde vamos puxar somente os dados ativos
+CREATE VIEW products_without_deleted AS
+SELECT * FROM products WHERE deleted_at IS NULL;
+
+--4. Renomear a nossa VIEW e a nossa TABLE
+ALTER TABLE products RENAME TO product_with_deleted;
+ALTER VIEW products_without_deleted RENAME TO products;
+
