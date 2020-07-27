193113439519aba523bf8369d4579c3d79a570e7
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-07-27 10:50:25 -0300

    Create orders.js

---
+++ AulaJS/AulaHtml/modulo08/src/routes/orders.js
@@ -0,0 +1,18 @@
+
+const express = require('express')
+const routes = express.Router()  // responsavel pelas rotas
+
+const OrderController = require('../app/controllers/OrderController')
+
+const{ onlyUsers } = require('../app/middlewares/session')
+
+
+routes.post('/', onlyUsers, OrderController.post)
+    .get('/', onlyUsers, OrderController.index)
+    .get('/sales', onlyUsers, OrderController.sales)
+    .get('/:id', onlyUsers, OrderController.show)
+    .post('/:id/:action', onlyUsers, OrderController.update)
+
+
+
+module.exports = routes // para exportar //routes
