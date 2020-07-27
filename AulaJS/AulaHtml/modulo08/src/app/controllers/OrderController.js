515c2dda804d180c83d7ee1b24c814794693eece
Taiz <62393795+TaizAlves@users.noreply.github.com>
2020-07-27 10:52:51 -0300

    Update OrderController.js

--- AulaJS/AulaHtml/modulo08/src/app/controllers/OrderController.js
+++ AulaJS/AulaHtml/modulo08/src/app/controllers/OrderController.js
@@ -1,7 +1,13 @@
 const LoadProductService = require('../services/LoadProductService')
+const LoadOrderService = require('../services/LoadOrderService')
 const User = require('../models/User')
+const Order = require('../models/Order')
 
+const Cart = require('../../lib/cart')
 const mailer = require('../../lib/mailer')
+const { update, findOne } = require('../models/Order')
+
+
 
 const email = (seller, product, buyer) =>`
 <h2>Olá ${seller.name}</h2>
@@ -22,34 +28,99 @@ const email = (seller, product, buyer) =>`
 `
 
 module.exports= {
+
+    async index(req, res){
+        
+        const orders = await LoadOrderService.load('orders', {
+            where: { buyer_id: req.session.userId}
+        })
+
+
+        return res.render("orders/index", {orders})
+
+    },
+
+    async sales(req, res){
+        
+        const sales = await LoadOrderService.load('orders', {
+            where: { seller_id: req.session.userId}
+        })
+
+
+        return res.render("orders/sales", {  sales })
+
+    },
+
+    async show(req, res){
+        const order = await LoadOrderService.load('order', {
+            where: { id: req.params.id }
+        })
+        return res.render("orders/details", { order })
+
+    },
+
     async post(req,res){
 
         try{
-        //pegar  os dados do produto
-            const product = await LoadProductService.load('product', { where: {
-                id: req.body.id
-            }})
+        //pegar os productos do carrinho
+                const  cart  = Cart.init(req.session.cart)
 
-        //pegar os dados do vendedor
-            const seller = await User.findOne( { where: { id: product.user_id}})
+                const buyer_id = req.session.userId
 
-        //pegar os dados do comprador
-            const buyer = await User.findOne( { where: { id: req.session.userId}})
+                const filteredItems = cart.items.filter(item =>
+                    item.product.user_id != buyer_id)
+            
+            //criar o pedido
+                const createOrderPromise = filteredItems.map(async item => {
+                let { product, price:total, quantity } = item
+                const { price, id:product_id, user_id: seller_id } = product
+                const status = "open"
+                
+                const order = await Order.create({
+                    seller_id,
+                    buyer_id,
+                    product_id,
+                    price,
+                    total,
+                    quantity,
+                    status
+                })
+
+                //pegar  os dados do produto
+                product = await LoadProductService.load('product', { where: {
+                    id: product_id
+                }})
+
+            //pegar os dados do vendedor
+                const seller = await User.findOne( { where: { id: seller_id}})
+
+            //pegar os dados do comprador
+                const buyer = await User.findOne( { where: { id: buyer_id}})
 
 
-        //enviar email com dados da compra para o vendedor
-            await mailer.sendMail( {
-                to: seller.email,
-                from: 'no-replay@lauchstore.com.br',
-                subject: 'Novo Pedido de Compra',
-                html:email(seller, product, buyer)
-            })
+            //enviar email com dados da compra para o vendedor
+                await mailer.sendMail( {
+                    to: seller.email,
+                    from: 'no-replay@lauchstore.com.br',
+                    subject: 'Novo Pedido de Compra',
+                    html:email(seller, product, buyer)
+                })
+
+                return order
+            
+
+         })
+
+        await Promise.all(createOrderPromise)
+
+        //limpar carrinho
+        delete req.session.cart
+        Cart.init()
+
 
         //notificar usuário com msg de sucesso ou erro
 
             return res.render('orders/success')
-
-
 
             
     
@@ -58,5 +129,47 @@ module.exports= {
             return res.render('orders/error')
 
         }
+    },
+
+    async update(req, res) {
+        try{
+            const { id, action } = req.params 
+
+            const accepetedActions = ['close', 'cancel']
+            if(!accepetedActions.includes(action)) return res.send("Can´t do this action")
+
+            //pegar o pedido
+            const order = await Order.findOne({
+                where: { id:id}
+            })
+
+            if (!order) return res.send("Order not found")
+
+            //verificar se ele está aberto
+
+            if(order.status != 'open') return res.send("Can´t do this action")
+
+            // atualizar o pedido
+
+            const statuses = {
+                close: "sold",
+                cancel: "cancelled"
+            }
+
+            order.status = statuses[action]
+
+            await Order.update(id, {
+                status: order.status
+            })
+
+            //redirecionar
+
+            return res.redirect('/orders/sales')
+
+
+        } catch(error){
+            console.error(error)
+        }
+
     }
 }
